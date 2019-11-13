207e20f3721a0c99ef024548655bf6df82d8173f
diwangs <27280516+diwangs@users.noreply.github.com>
2019-11-14 00:50:30 +0700

    Restructure

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 
 // insert contract ABI here
 import TrstToken from './TrstToken.json'
+import Vault from './Vault.json'
+import Staking from './Staking.json'
 
 class App extends Component {
   async componentDidMount() {
     await this.loadWeb3()
-    await this.loadTokenContract()
-    await this.getBalance()
+    await Promise.all([
+      this.loadContract(TrstToken),
+      this.loadContract(Vault),
+      this.loadContract(Staking)
+    ])
+    await this.testFunc()
   }
 
   // Connect with MetaMask
@@ -18,36 +24,42 @@ class App extends Component {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
-    }
-    else if (window.web3) {
+    } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
+    } else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
-  }
 
-  async loadTokenContract() {
-    const web3 = window.web3
-    
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-
-    // Load contract
-    const networkId = await web3.eth.net.getId()
-    const networkData = TrstToken.networks[networkId]
-    if(networkData) {
-      const tokenContract = new web3.eth.Contract(TrstToken.abi, networkData.address)
-      this.setState({ tokenContract })
-    } else {
-      window.alert('TrstToken contract not deployed to detected network.')
+    if (window.web3) {
+      const accounts = await window.web3.eth.getAccounts()
+      // window.web3.eth.defaultAccount = accounts[0] is defaultAccount deprecated?
+      // https://ethereum.stackexchange.com/questions/38365/web3-eth-defaultaccount-not-working
+      this.setState({ account: accounts[0] })
     }
   }
 
-  async getBalance() {
-    const balance = await this.state.tokenContract.methods.balance(this.state.account).call()
+  async loadContract(contractDefinition) {
+    const web3 = window.web3
+    const networkId = await web3.eth.net.getId()
+    const networkData = contractDefinition.networks[networkId]
+    if (networkData) {
+      const contractHandle = new web3.eth.Contract(contractDefinition.abi, networkData.address)
+      var newState = {}
+      newState[contractDefinition.contractName] = contractHandle
+      this.setState(newState)
+    } else {
+      window.alert(contractDefinition.contractName + ' contract not deployed to detected network.')
+    }
+  }
+
+  async testFunc() {
+    const balance = await this.state.TrstToken.methods.balance(this.state.account).call()
     this.setState({ balance })
+    await this.state.Vault.methods
+      .proposeLoan(1000000000, 1000000)
+      .send({from: this.state.account}) 
+      // due too error in defaultAccount, account must be added as argument everytime
   }
 
   constructor(props) {
@@ -61,9 +73,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            { this.state.balance
-              ? "Sampeyan punya " + this.state.balance / 1000 + " Trst Token"
-              : "Tunggu Bentar"
+            {this.state.balance
+              ? "Balance: " + this.state.balance / 1000 + " Trst Token"
+              : "Retrieving..."
             }
           </p>
           <a
